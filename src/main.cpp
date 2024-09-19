513e0b85358b4541114f3ce2508fbad550862e2f
Alwyn974 <alwyn974@gmail.com>
2024-09-19 21:41:44 +0200

    feat(exo3): Led blinking only if the button is not pressed

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <Arduino.h>
 
 const int ledPin = D1; // GPIO pin where the LED is connected
+const int buttonPin = D8; // GPIO pin where the button is connected
+
+bool ledState = LOW;
+bool lastButtonState = HIGH;
 
 void setup()
 {
+    Serial.begin(115200);
+
     pinMode(ledPin, OUTPUT);
+    pinMode(buttonPin, INPUT_PULLDOWN_16);
 }
 
 void blink()
@@ -17,5 +24,14 @@ void blink()
 
 void loop()
 {
-    blink();
+    bool buttonState = digitalRead(buttonPin);
+    Serial.printf("Button state: %d\n", buttonState);
+
+    if (buttonState != lastButtonState) {
+        ledState = !ledState;
+        lastButtonState = buttonState;
+    }
+
+    if (ledState == HIGH)
+        blink();
 }

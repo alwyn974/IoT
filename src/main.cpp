53aac6d0aea7f4457ba6f037743f371d90bb14b3
Alwyn974 <alwyn974@gmail.com>
2024-09-19 21:40:59 +0200

    feat(exo2): Led linked to a pin blinking

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
 
+const int ledPin = D1; // GPIO pin where the LED is connected
+
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+}
+
+void blink()
+{
+    digitalWrite(ledPin, HIGH); // turn the LED on
+    delay(500);                // wait for a second
+    digitalWrite(ledPin, LOW);  // turn the LED off
+    delay(500);                // wait for a second
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on
-    delay(500);                // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);  // turn the LED off
-    delay(500);                // wait for a second
+    blink();
 }
